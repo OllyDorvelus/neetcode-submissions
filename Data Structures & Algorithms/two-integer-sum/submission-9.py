c26a9016b1e1096aafdaf6fcba8da20dd7b6a60f
Olly <ollydorvelus@gmail.com>
2026-09-21 15:57:24 -0400

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        val_to_index_map: dict[int,int] = {}
+        for i in range(0, len(nums)):
+            num = nums[i]
+            val_to_index_map[num] = i
+        
+        for i in range(0, len(nums)):
+            num = nums[i]
+            val_to_find = target - num
+            if val_to_find in val_to_index_map and i != val_to_index_map[val_to_find]:
+                return [i, val_to_index_map[val_to_find]]
+        return []
